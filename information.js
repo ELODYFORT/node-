199ce5746de9f915e70df2bbdb070595989cedc4
ELODY FORT <elody.fort.professionnel@gmail.com>
2024-10-26 15:37:37 +0200

    exercice env

---
+++ information.js
@@ -0,0 +1,18 @@
+const name = 'John Doe';
+const age = 26;
+const passions = ['Ski', 'Hiking', 'Cinema'];
+
+console.log(name, age, passions);
+
+function displayPassions() {
+    console.log(`My passions are :`);
+    for (let i = 0; i < passions.length; i += 1) {
+      console.log(`- ${passions[i]}`);
+    }
+  }
+  
+  module.exports = {
+    name: name,
+    age: age,
+    displayPassions: displayPassions,
+  };
